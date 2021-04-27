5bad522ed95926f49bf5012ff55cff58a15701c0
Thomas Loubser <taloubser@gmail.com>
2021-04-27 20:33:44 +0100

    seed file with API to top movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Movie.destroy_all
+puts "Creating restaurants..."
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies["results"].each do |movie|
+  movie = Movie.new(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: movie["poster_path"],
+    rating: movie["vote_average"]
+  )
+  movie.save
+  puts "Created #{movie.title}"
+end
+
+puts "Finished!"
