d5b8ae933f16ae4388aea386f8e62b66e6935108
Thomas Loubser <taloubser@gmail.com>
2021-05-06 19:29:18 +0100

    added select2 to the movie drop down bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,15 +17,15 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap';
-
-
 import { loadDynamicBannerText } from '../components/banner';
 import { initSweetalert } from '../plugins/init_sweetalert';
+import { initSelect2 } from '../plugins/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   // [...]
   loadDynamicBannerText();
+  initSelect2();
   initSweetalert('.sweet-alert-demo', {
     title: "A nice alert",
     text: "This is a great alert, isn't it?",
